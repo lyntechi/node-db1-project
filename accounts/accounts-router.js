0c1dbf6d9b778f56bffc860e236be02a71e5f8d5
Lynda Santiago <lyntechi@gmail.com>
2020-09-08 21:11:56 -0400

    created endpoints for accounts but server isnt working

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -1,7 +1,50 @@
 const express = require("express");
-const db = require("../data/dbConfig")
+const db = require("../data/dbConfig");
 
-const router = express.Router()
+const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const accounts = await db
+      .select("*")
+      .from("accounts")
+      .where("id", req.params.id)
+      .limit(1);
+    res.json(accounts[0]);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const [id] = await db
+      .insert({
+        name: req.body.name,
+        budget: req.body.budget,
+      })
+      .into(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
+router.put("/", async (req, res, next) => {
+  await db("accounts")
+    .update({ name: req.body.name, budget: req.body.budget })
+    .where("id", req.params.id);
+});
+
+router.delete("/", async (req, res, next) => {
+  await db("accounts").where("id", req.params.id).del();
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const accountsRouter = require("../accounts/accounts-router");
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use("/accounts", accountsRouter);
 
 module.exports = server;
